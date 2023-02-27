70fbfcf374102b9236eb5eb32aca27d61ca28e9d
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-02-27 18:44:18 +0100

    VueJS VueCli (16:53) : include VueJS component

---
+++ vueJS_OC/super-compteur/src/components/Compteur.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Mon super compteur</h1>
+        <p @click="increment">Vous avez cliqué {{ total }} fois ! Super !</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Compteur',
+    data()
+    {
+        return {total:0}
+    },
+    methods:{
+        increment()
+        {
+            ++ this.total
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vueJS_OC/super-compteur/src/views/HomeView.vue
+++ vueJS_OC/super-compteur/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <Compteur />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Compteur from '@/components/Compteur.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld, Compteur
   }
 }
 </script>
