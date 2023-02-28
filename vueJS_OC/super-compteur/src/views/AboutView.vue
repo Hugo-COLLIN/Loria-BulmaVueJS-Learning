a992a0c1f7778d46aadde078960537525b970e66
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-02-28 11:16:07 +0100

    VueJS VueCli (22:25)

--- vueJS_OC/super-compteur/src/views/AboutView.vue
+++ vueJS_OC/super-compteur/src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+    </div>
+    <Compteur />
 </template>
+
+
+<script>
+import Compteur from "@/components/Compteur.vue";
+
+export default {
+    name: 'About',
+    components: {
+        Compteur
+    }
+}
+</script>
