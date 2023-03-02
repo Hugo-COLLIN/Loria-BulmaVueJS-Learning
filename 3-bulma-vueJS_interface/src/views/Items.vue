a78b3815f8720d4b467c2ffe94ad3040e7814550
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-02 17:35:55 +0100

    item (book) list

--- 3-bulma-vueJS_interface/src/views/Items.vue
+++ 3-bulma-vueJS_interface/src/views/Items.vue
@@ -45,197 +45,35 @@
     </nav>
 
     <div class="columns is-multiline">
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/tensorflow.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  TensorFlow For Machine Intelligence
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
+      <template v-for="(book, key) in books">
+        <div class="column is-12-tablet is-6-desktop is-4-widescreen">
+          <article class="box">
+            <div class="media">
+              <aside class="media-left">
+                <img :src="require(`@/assets/images/${book.coverImage}`)" width="80">
+              </aside>
+              <div class="media-content">
+                <p class="title is-5 is-spaced is-marginless">
+                  <router-link to="edit-item">{{book.title}}</router-link>
+                </p>
+                <p class="subtitle is-marginless">
+                  {{book.price}}
+                </p>
+                <div class="content is-small">
+                  {{book.pageCount}} pages
+                  <br>
+                  ISBN: {{book.ISBN}}
+                  <br>
+                  <router-link to="edit-item">Edit</router-link>
+                  <span>·</span>
+                  <a>Delete</a>
+                </div>
               </div>
             </div>
-          </div>
-        </article>
-      </div>
-
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box ">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/docker.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  Docker in production
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/gulp.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  Developing a Gulp.js Edge
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-
-      <template v-for="(book, key) in books" :key="book.id">
-        <div class="media" >
-          <div class="media-left is-marginless">
-            <p class="number">{{ key +1 }}</p>
-          </div>
-          <div class="media-left">
-            <img :src="require(`@/assets/${book.image}`)" width="40">
-          </div>
-          <div class="media-content">
-            <p class="title is-5 is-spaced is-marginless">
-              <router-link to="/edit-book">{{ book.name }}</router-link>
-            </p>
-          </div>
-          <div class="media-right">
-            {{ book.copiesSold}} sold
-          </div>
+          </article>
         </div>
       </template>
 
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/swift.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  Learning swift
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/js-framework.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  Choosing a Javascript framework
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-
-      <div class="column is-12-tablet is-6-desktop is-4-widescreen">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left">
-              <img src="images/google-cardboard.jpg" width="80">
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-spaced is-marginless">
-                <a href="edit-book.html">
-                  Destructuring Google Cardbord Apps
-                </a>
-              </p>
-              <p class="subtitle is-marginless">
-                $22.99
-              </p>
-              <div class="content is-small">
-                270 pages
-                <br>
-                ISBN: 9781939902351
-                <br>
-                <a href="edit-book.html">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
     </div>
 
     <nav class="pagination">
@@ -279,7 +117,7 @@ export default {
           price: "$22.99",
           pageCount: 270,
           ISBN: "9781939902351",
-          coverImage: "src/assets/images/tensorflow.jpg",
+          coverImage: "tensorflow.jpg",
           publishDate: 2017,
         },
         {
@@ -287,7 +125,7 @@ export default {
           price: "$22.99",
           pageCount: 156,
           ISBN: "9781939902184",
-          coverImage: "src/assets/images/docker.jpg",
+          coverImage: "docker.jpg",
           publishDate: 2015,
         },
         {
@@ -295,7 +133,7 @@ export default {
           price: "$24.99",
           pageCount: 342,
           ISBN: "9781939902115",
-          coverImage: "src/assets/images/swift.jpg",
+          coverImage: "swift.jpg",
           publishDate: 2015,
         },
         {
@@ -303,7 +141,7 @@ export default {
           price: "$19.99",
           pageCount: 270,
           ISBN: "9781939902092",
-          coverImage: "src/assets/images/js-framework.jpg",
+          coverImage: "js-framework.jpg",
           publishDate: 2016,
         },
         {
@@ -311,7 +149,7 @@ export default {
           price: "$15.99",
           pageCount: 134,
           ISBN: "9781939902146",
-          coverImage: "src/assets/images/gulp.jpg",
+          coverImage: "gulp.jpg",
           publishDate: 2014,
         },
       ],
@@ -321,7 +159,7 @@ export default {
           price: "$22.99",
           pageCount: 270,
           ISBN: "9781939902351",
-          coverImage: "src/assets/images/tensorflow.jpg",
+          coverImage: "tensorflow.jpg",
           publishDate: 2017,
         },
         {
@@ -329,7 +167,7 @@ export default {
           price: "$22.99",
           pageCount: 156,
           ISBN: "9781939902184",
-          coverImage: "src/assets/images/docker.jpg",
+          coverImage: "docker.jpg",
           publishDate: 2015,
         },
         {
@@ -337,7 +175,7 @@ export default {
           price: "$22.99",
           pageCount: 342,
           ISBN: "9781939902115",
-          coverImage: "src/assets/images/swift.jpg",
+          coverImage: "swift.jpg",
           publishDate: 2015,
         },
         {
@@ -345,7 +183,7 @@ export default {
           price: "$19.99",
           pageCount: 270,
           ISBN: "9781939902092",
-          coverImage: "src/assets/images/js-framework.jpg",
+          coverImage: "js-framework.jpg",
           publishDate: 2016,
         },
         {
@@ -353,7 +191,7 @@ export default {
           price: "$22.99",
           pageCount: 134,
           ISBN: "9781939902146",
-          coverImage: "src/assets/images/gulp.jpg",
+          coverImage: "gulp.jpg",
           publishDate: 2014,
         },
       ],
