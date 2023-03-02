e1647b2cb5c1aeed1eeaf4433b1f5073ebe23522
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-02 12:09:13 +0100

    login page full screen

--- 3-bulma-vueJS_interface/src/App.vue
+++ 3-bulma-vueJS_interface/src/App.vue
@@ -1,103 +1,106 @@
 <template>
-  <nav class="navbar has-shadow">
-    <div class="navbar-brand">
-      <a class="navbar-item">
-        <img src="./assets/logo.png">
-      </a>
-      <div class="navbar-burger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <div class="navbar-item">
-          <small>Publishing at the speed of technology</small>
+  <template v-if="$route.name !== 'login'">
+    <nav class="navbar has-shadow">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="./assets/logo.png">
+        </a>
+        <div class="navbar-burger">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
-      <div class="navbar-end">
-        <div class="navbar-item has-dropdown is-hoverable">
-          <div class="navbar-link">
-            Alex Johnson
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <small>Publishing at the speed of technology</small>
           </div>
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              <div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <div class="navbar-link">
+              Alex Johnson
+            </div>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                <div>
                          <span class="icon is-small">
                             <i class="fa fa-user-circle-o"></i>
                          </span>
-                Profile
-              </div>
-            </a>
+                  Profile
+                </div>
+              </a>
 
-            <a class="navbar-item">
-              <div>
+              <a class="navbar-item">
+                <div>
                              <span class="icon is-small">
                                 <i class="fa fa-bug"></i>
                              </span>
-                Report bug
-              </div>
-            </a>
+                  Report bug
+                </div>
+              </a>
 
-            <a class="navbar-item">
-              <div>
+              <a class="navbar-item">
+                <div>
                              <span class="icon is-small">
                                 <i class="fa fa-sign-out"></i>
                              </span>
-                Sign Out
-              </div>
-            </a>
-          </div>
+                  Sign Out
+                </div>
+              </a>
+            </div>
 
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-  <section class="section">
-    <div class="columns">
-      <div class="column is-4-tablet is-3-desktop is-2-widescreen">
-        <aside class="menu">
-          <p class="menu-label">Menu</p>
-          <ul class="menu-list">
-            <li>
-              <router-link to="/dashboard">
+    <section class="section">
+      <div class="columns">
+        <div class="column is-4-tablet is-3-desktop is-2-widescreen">
+          <aside class="menu">
+            <p class="menu-label">Menu</p>
+            <ul class="menu-list">
+              <li>
+                <router-link to="/dashboard">
               <span class="icon ">
                 <i class="fa fa-tachometer"></i>
               </span>Dashboard</router-link>
-            </li>
-            <li>
-              <router-link to="/items">
+              </li>
+              <li>
+                <router-link to="/items">
               <span class="icon">
                 <i class="fa fa-book"></i>
               </span> Items
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/customers">
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/customers">
               <span class="icon">
                 <i class="fa fa-address-book"></i>
               </span> Customers
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/orders">
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/orders">
               <span class="icon">
                 <i class="fa fa-file-text-o"></i>
               </span>
-                Orders
-              </router-link>
-            </li>
-          </ul>
-        </aside>
+                  Orders
+                </router-link>
+              </li>
+            </ul>
+          </aside>
 
+        </div>
+        <main class="column">
+          <router-view/>
+        </main>
       </div>
-      <main class="column">
-        <router-view/>
-      </main>
-    </div>
-  </section>
+    </section>
+  </template>
+  <template v-else><router-view/></template>
 </template>
 
 <style lang="scss">
--- 3-bulma-vueJS_interface/src/views/Login.vue
+++ 3-bulma-vueJS_interface/src/views/Login.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="dashboard">
-    <h1>This is the dashboard page</h1>
+  <div class="login">
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form class="box">
+
+                <div class="field has-text-centered">
+                  <img :src="require('@/assets/images/logo-bis.png')" width="167">
+                </div>
+
+                <div class="field">
+                  <label class="label">Email</label>
+                  <div class="control has-icons-left">
+                    <input class="input" type="email" placeholder="e.g. alexjohnson@gmail.com">
+                    <span class="icon is-small is-left">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">Password</label>
+                  <div class="control has-icons-left">
+                    <input class="input" type="password" placeholder="**********">
+                    <span class="icon is-small is-left">
+                                        <i class="fa fa-lock"></i>
+                                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                  </label>
+                </div>
+
+                <div class="field">
+                  <button class="button is-success">
+                    Login
+                  </button>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Dashboard'
+  name: 'Login'
 }
 </script>
 
