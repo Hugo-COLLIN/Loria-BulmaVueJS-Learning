3c791bb760ee0ea36fff2b509c3ffd2017be341b
Sacus <75908397+sacus1@users.noreply.github.com>
2023-03-08 16:01:23 +0100

    Test init

---
+++ 4-bulma-vueJS_interface_api/tests/codecept.conf.js
@@ -0,0 +1,24 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:8082/',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'tests'
+}
---
+++ 4-bulma-vueJS_interface_api/tests/tracks_test.js
@@ -0,0 +1,5 @@
+Feature('tracks');
+
+Scenario('test something',  ({ I }) => {
+
+});
