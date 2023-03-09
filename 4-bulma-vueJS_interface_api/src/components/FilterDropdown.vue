d6692535b3ca8a9c262eae185bdda6940c7f7224
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-09 18:57:52 +0100

    filters not working

---
+++ 4-bulma-vueJS_interface_api/src/components/FilterDropdown.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="level-item">
+    Order by
+  </div>
+  <div class="level-item">
+    <div class="select">
+      <select @change="sortItems">
+        <option v-for="option in listOptions" :value="option.shortName">{{ option.fullName }}</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterDropdown",
+  emits: ["filter-update"],
+  data() {
+    return {
+      listOptions: {}
+    };
+  },
+  methods: {
+    sortItems(event) {
+      this.$emit("filter-update", event.target.value);
+      // console.log("Event : " + event.target.value);
+    },
+
+    createFilters(list) {
+      this.listOptions = list;
+    },
+
+    // createFilter(name, short)
+    // {
+    //   this.listOptions.push({fullName: name, shortName: short});
+    // }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
