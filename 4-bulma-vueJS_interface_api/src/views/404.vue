a828ec8e169da2c753751389f560789fffc8d7ae
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-10 11:08:37 +0100

    page layout

--- 4-bulma-vueJS_interface_api/src/views/404.vue
+++ 4-bulma-vueJS_interface_api/src/views/404.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
     <h2 class="title">Seems you are in troubles...</h2>
-    <router-link to="/" class="subtitle is-underlined">↩ Let's go back to safe place</router-link>
+    <router-link to="/" class="subtitle is-underlined">↩ Let's go back to a safer place</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: '404',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
