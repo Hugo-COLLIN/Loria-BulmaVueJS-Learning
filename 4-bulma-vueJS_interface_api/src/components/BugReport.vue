ca8cde4ba82699d40745e32170b95cb308e54cf6
Poirot Lucas <lucas.poirot@loria.fr>
2023-03-10 10:07:28 +0100

    Commentaire components Bug et login

--- 4-bulma-vueJS_interface_api/src/components/BugReport.vue
+++ 4-bulma-vueJS_interface_api/src/components/BugReport.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="modal" :class="{'is-active': showModal}">
+  <div :class="{'is-active': showModal}" class="modal">
     <div class="modal-background" @click="closeModal"></div>
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Modal title</p>
-        <button class="delete" aria-label="close" @click="closeModal"></button>
+        <button aria-label="close" class="delete" @click="closeModal"></button>
       </header>
       <section class="modal-card-body">
-        <div class="notification is-success" :class="{'is-hidden': hideNotification}">
+        <div :class="{'is-hidden': hideNotification}" class="notification is-success">
           <p>
             <span class="icon"><i class="fa fa-bug"></i></span>
             Thanks. Your bug has been reported.
           </p>
           <p>We will do our best to fix it as soon as possible</p>
         </div>
-        <p class="help" :class="{'is-hidden': hideNotification}">The following message was sent</p>
-        <textarea class="textarea" placeholder="Let us know what problems you faced." :disabled="!hideNotification" v-model="reportMessage"></textarea>
+        <p :class="{'is-hidden': hideNotification}" class="help">The following message was sent</p>
+        <textarea v-model="reportMessage" :disabled="!hideNotification" class="textarea"
+                  placeholder="Let us know what problems you faced."></textarea>
       </section>
       <footer class="modal-card-foot">
         <button class="button is-success" @click="sendReport">Save changes</button>
@@ -28,19 +29,23 @@
 <script>
 export default {
   name: "BugReport",
+  // the data that will be used in the component
   data() {
     return {
       hideNotification: true,
       reportMessage: ""
     };
   },
+  // the properties that will be used in the component
   props: {
+    // the showModal property will be used to show or hide the popup
     showModal: {
       type: Boolean,
       default: false
     }
   },
   methods: {
+
     closeModal() {
       this.$emit("close");
     },
@@ -53,6 +58,7 @@ export default {
     sendReport() {
       this.hideNotification = false;
 
+      // TODO: Explain what is happening here
       setTimeout(() => {
         this.hideNotification = true;
         this.resetModal();
--- 4-bulma-vueJS_interface_api/src/components/LoginMechanics.vue
+++ 4-bulma-vueJS_interface_api/src/components/LoginMechanics.vue
@@ -13,21 +13,26 @@ export default {
      * Log in the user
      */
     login(email, password) {
-      // TODO : Mettre un message d'erreur correcte
+      // TODO : Mettre un message d'erreur correct a la place des alert
+      // Check if the email and password are not empty
       if (!email || !password){
         alert("Please enter email and password");
         return
       }
+      // Ask the API to log in the user
       axios.post('http://51.91.76.245:8000/api/login', {
         username: email,
         password: password,
       })
           .then(response => {
+            // save the token in the session storage
             sessionStorage.setItem('tokenSession', response.data);
+            // redirect to the dashboard
             this.$router.push({name: 'dashboard'});
           })
           .catch(error => {
-            // TODO : Same as above
+            // TODO : Remplacer les alert par des messages d'erreur
+            // If the email or password is incorrect , then the API return a 401 error
             if (error.response.status === 401)
               alert("Please enter a valid password and email");
             else
@@ -39,9 +44,11 @@ export default {
      */
     loginStateRouter()
     {
-      let res = false;
+      let isLogin = false;
       // console.log(sessionStorage.getItem('tokenSession'));
+      // if the token is null then the user is not logged in
       if (sessionStorage.getItem('tokenSession') !== null) {
+        // Ask the API if the token is valid
         axios.get('http://51.91.76.245:8000/api/login', {
           headers: {
             token: sessionStorage.getItem('tokenSession')
@@ -49,18 +56,21 @@ export default {
         })
             .then((response) => {
               console.log(response);
-              res = true;
+              // If the token is valid then the user is logged in
+              isLogin = true;
             })
             .catch((error) => {
               console.log(error);
+              // If the token is not valid then the user is not logged in and the token is deleted
               sessionStorage.removeItem('tokenSession');
+              // redirect to the login page
               router.push({name: 'login'});
               // alert("Disconnected");
-              res = false;
+              isLogin = false;
             });
       }
 
-      return res;
+      return isLogin;
     },
 
     /**
@@ -69,21 +79,25 @@ export default {
     logout()
     {
       console.log(sessionStorage.getItem('tokenSession'))
+      // Ask the API to delete the token to log out the user
       axios.delete('http://51.91.76.245:8000/api/login', {
         headers: {
           token: sessionStorage.getItem('tokenSession')
         }
       })
           .then(() => {
-            //message = "Loggged out successfully";
+            //message = "Logged out successfully";
+            // redirect to the login page
             this.loginRequired();
           })
-          .catch((error) => {
-            console.log(error);
-          });
+          .catch((error) => console.log(error));
+      // redirect to the login page
       this.loginRequired();
     },
 
+    /**
+     * Redirect to the login page
+     */
     loginRequired()
     {
       sessionStorage.removeItem('tokenSession');
