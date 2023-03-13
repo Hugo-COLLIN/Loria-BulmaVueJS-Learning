2b000011b2b4b01a72936e17c6a0411348985d48
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-13 17:10:59 +0100

    Custom ViewList parameters

--- 4-bulma-vueJS_interface_api/src/views/Items.vue
+++ 4-bulma-vueJS_interface_api/src/views/Items.vue
@@ -7,8 +7,25 @@ import ViewList from "@/components/ViewList.vue";
 export default {
   name: 'Items',
   components: {ViewList},
+  data()
+  {
+    return {
+      sorts: [
+        {fullName: "Name", shortName: "Name"},
+        {fullName: "Unit Price", shortName: "UnitPrice"},
+        {fullName: "Duration", shortName: "Milliseconds"},
+        {fullName: "Composer", shortName: "Composer"},
+        {fullName: "Track Id", shortName: "TrackId"},
+        {fullName: "Album Id", shortName: "AlbumId"},
+        {fullName: "Genre Id", shortName: "GenreId"},
+        {fullName: "Media Type", shortName: "MediaTypeId"},
+      ],
+    }
+  },
   mounted() {
-    this.$refs.viewList.init("http://51.91.76.245:8000/api/tracks")
+    this.$refs.viewList.setSortOptions(this.sorts);
+    this.$refs.viewList.setDataSet("http://51.91.76.245:8000/api/tracks");
+
   },
 }
 </script>
