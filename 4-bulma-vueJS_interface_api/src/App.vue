16dae0c7702909ff8421e1d9489050e77760ff22
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-13 10:27:27 +0100

    header bar externalisation

--- 4-bulma-vueJS_interface_api/src/App.vue
+++ 4-bulma-vueJS_interface_api/src/App.vue
@@ -1,64 +1,9 @@
 <template>
   <template v-if="$route.name !== 'login'">
-    <LoginMechanics ref="loginMechanics"></LoginMechanics>
-    <nav class="navbar has-shadow">
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <img src="./assets/logo.png" alt="logo">
-        </a>
-        <div class="navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <div class="navbar-item">
-            <small>Publishing at the speed of technology</small>
-          </div>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <div class="navbar-link">
-              Alex Johnson
-            </div>
-            <div class="navbar-dropdown">
-              <a class="navbar-item">
-                <div>
-                <span class="icon is-small">
-                   <i class="fa fa-user-circle-o"></i>
-                </span>
-                  Profile
-                </div>
-              </a>
-
-              <a class="navbar-item"  @click="openBugReport = true">
-                <div>
-                <span class="icon is-small">
-                   <i class="fa fa-bug"></i>
-                </span>
-                  Report bug
-                </div>
-              </a>
-
-              <a class="navbar-item" @click="this.$refs.loginMechanics.logout">
-                <div>
-                <span class="icon is-small">
-                   <i class="fa fa-sign-out"></i>
-                </span>
-                  Sign Out
-                </div>
-              </a>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </nav>
+    <HeaderBar @open-bug-report="bugReport"/>
 
     <section class="section">
-      <div class="columns"> <!-- vv SideMenu vv -->
+      <div class="columns">
         <div class="column is-4-tablet is-3-desktop is-2-widescreen" v-if="$route.name !== '404'">
           <SideMenu ref="sideMenu"/>
         </div>
@@ -74,15 +19,15 @@
 
 <script>
 import BugReport from "@/components/BugReport.vue";
-import LoginMechanics from "@/components/LoginMechanics.vue";
 import SideMenu from "@/components/SideMenu.vue";
+import HeaderBar from "@/HeaderBar.vue";
 
 export default {
   name: "App",
   components: {
+    HeaderBar,
     SideMenu,
     BugReport,
-    LoginMechanics
   },
   data()
   {
@@ -119,6 +64,13 @@ export default {
     }
   },
   methods: {
+    /**
+     * Open "Bug report modal"
+     */
+    bugReport(status) {
+      console.log(status)
+      this.openBugReport = status;
+    }
 
   },
   mounted() {
---
+++ 4-bulma-vueJS_interface_api/src/HeaderBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <LoginMechanics ref="loginMechanics"></LoginMechanics>
+  <nav class="navbar has-shadow">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="./assets/logo.png" alt="logo">
+      </a>
+      <div class="navbar-burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <small>Publishing at the speed of technology</small>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <div class="navbar-link">
+            Alex Johnson
+          </div>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              <div>
+                <span class="icon is-small">
+                   <i class="fa fa-user-circle-o"></i>
+                </span>
+                Profile
+              </div>
+            </a>
+
+            <a class="navbar-item" @click="bugReport">
+              <div>
+                <span class="icon is-small">
+                   <i class="fa fa-bug"></i>
+                </span>
+                Report bug
+              </div>
+            </a>
+
+            <a class="navbar-item" @click="this.$refs.loginMechanics.logout">
+              <div>
+                <span class="icon is-small">
+                   <i class="fa fa-sign-out"></i>
+                </span>
+                Sign Out
+              </div>
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+import LoginMechanics from "@/components/LoginMechanics.vue";
+import bugReport from "@/components/BugReport.vue";
+
+export default {
+  name: 'HeaderBar',
+  components: {LoginMechanics},
+  emits: ['open-bug-report'],
+  data() {
+    return {
+      openBugReport: false
+    }
+  },
+  methods: {
+    bugReport() {
+      console.log('bugReport');
+      this.$emit('open-bug-report', true);
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+nav {
+
+}
+</style>
