ee1bf9d31637c0a9d542215392af2bbfa060960a
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-03-17 18:47:32 +0100

    formular externalisation try

--- 4-bulma-vueJS_interface_api/src/components/modals/ModalItem.vue
+++ 4-bulma-vueJS_interface_api/src/components/modals/ModalItem.vue
@@ -7,6 +7,8 @@
         <p class="modal-card-title">Item information</p>
         <button class="delete" aria-label="close" @click="closeModal"></button>
       </header>
+
+<!--      <Formular :form-struct="formStruct" :hide-notification="hideNotification"/>-->
       <form ref="itemAddForm">
         <section class="modal-card-body" :class="{'notification is-success': !hideNotification}">
           <div :class="{'is-hidden': hideNotification}">
@@ -14,52 +16,56 @@
           </div>
 
           <div :class="{'is-hidden': !hideNotification}">
-              <template v-for="(item,key) in formStruct">
-                <div class="field">
-                  <div class="columns is-desktop">
-                    <template v-for="(item2,key2) in item">
-                      <div class="column">
-                        <label class="label">{{ item2.label }}</label>
-                        <div class="control" :class="{'has-icons-left': item2.icon !== '' && item2.icon !== undefined }">
-                          <input :name="key2" v-model="this.form[item2.name]" class="input" :class="item2.classes, {'is-danger' : this.error[item2.name]}" :placeholder="item2.placeholder" required :type="item2.type">
-                          <span v-if="item2.icon !== '' && item2.icon !== undefined" class="icon is-small is-left">
+            <template v-for="(item,key) in formStruct">
+              <div class="field">
+                <div class="columns is-desktop">
+                  <template v-for="(item2,key2) in item">
+                    <div class="column">
+                      <label class="label">{{ item2.label }}</label>
+                      <div class="control" :class="{'has-icons-left': item2.icon !== '' && item2.icon !== undefined }">
+                        <input :name="key2" v-model="this.form[item2.name]" class="input"
+                               :class="item2.classes, {'is-danger' : this.error[item2.name]}"
+                               :placeholder="item2.placeholder" required :type="item2.type">
+                        <span v-if="item2.icon !== '' && item2.icon !== undefined" class="icon is-small is-left">
                             <i :class="item2.icon"></i>
                           </span>
-                        </div>
-                        <p v-if="this.error[item2.name]" class="help is-danger">Please enter {{ item2.label.toLowerCase() }}</p>
                       </div>
-                    </template>
-                  </div>
+                      <p v-if="this.error[item2.name]" class="help is-danger">Please enter {{ item2.label.toLowerCase()
+                        }}</p>
+                    </div>
+                  </template>
                 </div>
-              </template>
+              </div>
+            </template>
 
-<!--            <div class="field">-->
-<!--              <label class="label">Cover image</label>-->
-<!--              <div class="control">-->
-<!--                <div class="file has-name">-->
-<!--                  <label class="file-label">-->
-<!--                    <input class="file-input" type="file"> &lt;!&ndash; v-model="this.form.title"&ndash;&gt;-->
-<!--                    <span class="file-cta">-->
-<!--                       <span class="file-icon">-->
-<!--                        <i class="fa fa-upload"></i>-->
-<!--                       </span>-->
-<!--                         <span class="file-label">-->
-<!--                          Choose a file…-->
-<!--                         </span>-->
-<!--                       </span>-->
+            <!--            <div class="field">-->
+            <!--              <label class="label">Cover image</label>-->
+            <!--              <div class="control">-->
+            <!--                <div class="file has-name">-->
+            <!--                  <label class="file-label">-->
+            <!--                    <input class="file-input" type="file"> &lt;!&ndash; v-model="this.form.title"&ndash;&gt;-->
+            <!--                    <span class="file-cta">-->
+            <!--                       <span class="file-icon">-->
+            <!--                        <i class="fa fa-upload"></i>-->
+            <!--                       </span>-->
+            <!--                         <span class="file-label">-->
+            <!--                          Choose a file…-->
+            <!--                         </span>-->
+            <!--                       </span>-->
 
-<!--                    <span class="file-name">-->
-<!--                       No file chosen-->
-<!--                    </span>-->
-<!--                  </label>-->
-<!--                  <p class="help is-danger" v-if="error.coverImage">Please choose a picture</p>-->
-<!--                </div>-->
-<!--              </div>-->
+            <!--                    <span class="file-name">-->
+            <!--                       No file chosen-->
+            <!--                    </span>-->
+            <!--                  </label>-->
+            <!--                  <p class="help is-danger" v-if="error.coverImage">Please choose a picture</p>-->
+            <!--                </div>-->
+            <!--              </div>-->
 
-<!--            </div>-->
+            <!--            </div>-->
           </div>
         </section>
       </form>
+
       <footer class="modal-card-foot field" :class="{'is-hidden': !hideNotification}">
         <div class="buttons">
           <button class="button is-medium is-success" @click.prevent="sendModal">{{this.btnTitle}}</button>
@@ -76,8 +82,11 @@
 /*
   This component is used to create a new item or edit an existing one.
  */
+import Formular from "@/components/modals/Formular.vue";
+
 export default {
   name: "ModalItem",
+  components: {Formular},
   data() {
     return {
       hideNotification: true,
